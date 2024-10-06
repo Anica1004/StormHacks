b6c201aee3d121d6588b5d51d8e65d42f58d5028
Anica1004 <anicamok@gmail.com>
2024-10-05 19:10:47 -0700

    built the login page

---
+++ HouseMate/app/(tabs)/login.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { StyleSheet, Image, Platform } from 'react-native';
+
+export default function login() {
+
+return(
+    <View>
+      
+    </View>
+); 
+}
+
+const styles = StyleSheet.create({
+   
+  });
+  
